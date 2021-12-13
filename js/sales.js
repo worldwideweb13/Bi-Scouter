ea980183c66423a29f4c89f1e0d87d8c3f7c6999
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-12-13 16:23:41 +0900

    fix:非同期処理修正

--- js/sales.js
+++ js/sales.js
@@ -5,37 +5,25 @@ function insertTable(start_date,last_date){
       last_date: last_date,    
   };
   axios.post(url,params).then(res => {
-    console.log(res.data);
-  })
-  .catch(error => {
-    console.log(error);
+    res.data.result.forEach(key => {
+        console.log("OK?");     
+        console.log(key);
+        //const wrapper = $('#js-myTable')
+        const wrapper = document.getElementById('js-myTable')
+        const html = `
+        <tr>
+          <td id="td_1">${key["ts_date"]}</td>
+          <td id="td_2"><a href="https://delta-tracer.com/item/detail/jp/${key["asin"]}">${key["product_name"]}</a></td>
+          <td id="td_3">${key["sku"]}</td>
+          <td id="td_4">${key["sales_stock"]}</td>
+          <td id="td_5">￥${key["txSales"]}</td>
+          <td id="td_6">￥${key["purchase_price"]}</td>
+          <td id="td_7">￥${key["profit"]}</td>
+        </tr>  
+        `;
+        wrapper.insertAdjacentHTML('beforeend',html)    
+    });
   });
-
-    // axios.post('ファイル名',{key:val})...json形式でdataを送る
-    // const res = axios.post('../php/sales_select.php', {
-    //     start_date: start_date,
-    //     last_date: last_date,
-    //   }).then(res => {
-    //       res.data.result.forEach(key => {
-    //           console.log("OK?");     
-    //           console.log(key);
-    //           //const wrapper = $('#js-myTable')
-    //           const wrapper = document.getElementById('js-myTable')
-    //           const html = `
-    //           <tr>
-    //             <td id="td_1">${key["ts_date"]}</td>
-    //             <td id="td_2"><a href="https://delta-tracer.com/item/detail/jp/${key["asin"]}">${key["product_name"]}</a></td>
-    //             <td id="td_3">${key["sku"]}</td>
-    //             <td id="td_4">${key["sales_stock"]}</td>
-    //             <td id="td_5">￥${key["txSales"]}</td>
-    //             <td id="td_6">￥${key["purchase_price"]}</td>
-    //             <td id="td_7">￥${key["profit"]}</td>
-    //          </tr>  
-    //           `;
-    //         //   wrapper.append(html)
-    //           wrapper.insertAdjacentHTML('beforeend',html)
-    //       });
-    // });    
 }
 
 document.getElementById("submit_button").onclick = function(){
@@ -46,8 +34,6 @@ document.getElementById("submit_button").onclick = function(){
     wrapper.innerHTML = "";
     insertTable(start_date,last_date);
 }
-
-//   </foreach>
 
 
 
